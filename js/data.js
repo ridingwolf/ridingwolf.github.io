bc2d610dc78fcd5284aa17b634f5e59f92d9b17e
RidingWolf <cybercaveman@gmail.com>
2016-03-16 00:59:47 +0100

    commit raw data

---
+++ js/data.js
@@ -0,0 +1,13 @@
+// nicked from http://codepen.io/KryptoniteDove/post/load-json-file-locally-using-pure-javascript
+(function loadResumeData() {
+    var request = new XMLHttpRequest();
+    request.overrideMimeType("application/json");
+    request.open('GET', './Curriculum_vitae.json', false); 
+    request.onreadystatechange = function () {
+          if (request.readyState == 4 && request.status == "200") {
+            window.resume = JSON.parse(request.responseText);
+            alert("success");
+        }
+    };
+    request.send(null);  
+})();
